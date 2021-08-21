fe0bab0894e635835ad67288de423fd3ed8e0b93
Your Nikola <nikolag857@gmail.com>
2021-08-21 12:13:23 +0200

    ubacen maven

---
+++ rest-web-services/rest-web-services/src/test/java/com/NJTProject/rest/webservices/restwebservices/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NJTProject.rest.webservices.restwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
