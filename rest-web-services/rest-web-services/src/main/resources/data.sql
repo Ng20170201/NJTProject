4df2dc496a54fa66f7dec6d99663ed4a141a163a
Andjela Filipovic <afilipovic@uniondome.com>
2021-09-02 22:50:14 +0200

    patients

--- rest-web-services/rest-web-services/src/main/resources/data.sql
+++ rest-web-services/rest-web-services/src/main/resources/data.sql
@@ -7,7 +7,7 @@
  * Author:  Andjela
  * Created: Sep 2, 2021
  */
-
+/*
 insert into patient(id,ucin,name,surname,birth_date,email,telephone,password)
 values(1111111,'111','pacijent','surname',parsedatetime('17-09-2012 18:47:52.69', 'dd-MM-yyyy hh:mm:ss.SS'),'jpa@gmail.com','069625748','123');
 
@@ -16,3 +16,4 @@ values(11112222,'222','pacijent2','surname',parsedatetime('17-09-2012 18:47:52.6
 
 insert into patient(id,ucin,name,surname,birth_date,email,telephone,password)
 values(100022220002,'333','pacijent3','surname',parsedatetime('17-09-2012 18:47:52.69', 'dd-MM-yyyy hh:mm:ss.SS'),'jpa3@gmail.com','0699999','123');
+*/
